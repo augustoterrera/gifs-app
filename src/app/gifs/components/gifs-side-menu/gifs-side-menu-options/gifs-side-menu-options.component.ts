9cdb9c2a2a29d63f51489d9b04a645f883e2e69a
Terrera Augusto <augustoterrera@gmail.com>
2025-04-15 11:51:00 -0300

    gifs-app

---
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOptions{
+  icon: string;
+  label: string;
+  route: string;
+  subLabel: string;
+}
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './gifs-side-menu-options.component.html',
+})
+export class GifsSideMenuOptionsComponent { 
+
+  menuOptions: MenuOptions[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      route: '/dashboard/trending',
+      subLabel: 'Gifs Populares'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      route: '/dashboard/search',
+      subLabel: 'Buscar gifs'
+    },
+  ]
+
+}
